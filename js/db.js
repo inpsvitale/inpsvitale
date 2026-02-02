776b7d93fba4bdbc86644918201a0b5c5e6f8525
inpsvitale <inpsvitale@gmail.com>
2026-02-02 15:19:13 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -2,6 +2,19 @@ import { db } from "./firebase.js";
 import { collection, getDocs, addDoc } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+import {
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+export async function getPlayers() {
+  const snap = await getDocs(collection(db, "players"));
+  return snap.docs.map(d => ({ id: d.id, ...d.data() }));
+}
+
 export async function getMatches() {
   const snap = await getDocs(collection(db,"matches"));
   return snap.docs.map(d => ({ id:d.id, ...d.data() }));
