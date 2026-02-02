8cd747f48a9cc1dd43a1b3733e7ec8d43780a22f
inpsvitale <inpsvitale@gmail.com>
2026-02-02 14:54:41 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCgK9cHVcaIlYQriPYzO9x_UWjZEpDs17w",
+    authDomain: "inpsvitale-1b1bf.firebaseapp.com",
+    projectId: "inpsvitale-1b1bf",
+    storageBucket: "inpsvitale-1b1bf.firebasestorage.app",
+    messagingSenderId: "1078528600289",
+    appId: "1:1078528600289:web:4ae76f5835023e5a652fa0"
+};
+
 export const app = initializeApp({
   apiKey: "API_KEY",
   authDomain: "PROJECT.firebaseapp.com",
