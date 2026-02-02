44f118ab2acc4c2b32b6fe078891d7f6bcfcc4b1
inpsvitale <inpsvitale@gmail.com>
2026-02-02 15:08:35 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,13 +3,21 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgK9cHVcaIlYQriPYzO9x_UWjZEpDs17w",
-    authDomain: "inpsvitale-1b1bf.firebaseapp.com",
-    projectId: "inpsvitale-1b1bf",
-    storageBucket: "inpsvitale-1b1bf.firebasestorage.app",
-    messagingSenderId: "1078528600289",
-    appId: "1:1078528600289:web:4ae76f5835023e5a652fa0"
+
+  apiKey: "AIzaSyCgK9cHVcaIlYQriPYzO9x_UWjZEpDs17w",
+
+  authDomain: "inpsvitale-1b1bf.firebaseapp.com",
+
+  projectId: "inpsvitale-1b1bf",
+
+  storageBucket: "inpsvitale-1b1bf.firebasestorage.app",
+
+  messagingSenderId: "1078528600289",
+
+  appId: "1:1078528600289:web:4ae76f5835023e5a652fa0"
+
 };
+
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
